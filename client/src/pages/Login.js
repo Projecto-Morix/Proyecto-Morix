aac8299b93e407f67956d962c794ea43eb265bf0
yepecpp <jhonmiguel.1@hotmail.com>
2022-06-08 13:51:02 -0400

    Login

---
+++ client/src/pages/Login.js
@@ -0,0 +1,13 @@
+import LoginC from './components/LoginC';
+function Login() {
+  return (
+    <div class="login-panel">
+    <div class="login-header">
+        <div class="header-title">Bienvenido</div>
+        <div class="header-subtitle">Iniciar Sesión con tu Cuenta</div>
+    </div>
+    <LoginC/>
+</div>
+  );
+}
+export default Login;
---
+++ client/src/pages/components/LoginC.jsx
@@ -0,0 +1,12 @@
+function LoginC() {
+    return(
+<form action="" method="">
+        <label for="username">Usuario</label>
+        <input type="text" id="username"/>
+        <label for="password">Contraseña</label>
+        <input type="password"/>
+        <a href="">Olvide mi contraseña</a>
+        <input type="submit"/>
+</form>)
+}
+export default LoginC;
