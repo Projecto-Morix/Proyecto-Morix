70ba274fc6d4446e16f429cda8a9eeb1209bf3e1
Bob <bryanmoreno2005@gmail.com>
2022-06-22 09:56:22 -0400

    Saving Aidan

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -25,7 +25,12 @@ router.post('/register', (req, res) => {
 
     let userData = req.body;
 
-    con.query(`INSERT INTO Usuarios VALUES`)
+    con.query(`INSERT INTO Usuarios VALUES(null, '${userData.nombre}', '${userData.apellido}', ${userData.password}, '${userData.email}', '${userData.telefono}', ${userData.fecha}, ${userData.imagen})`, (err, result) => {
+            
+        if (err) throw err;
+
+        //res.send(result);
+    });
 });
 
 module.exports = router;
