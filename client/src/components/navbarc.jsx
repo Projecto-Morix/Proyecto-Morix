2b0e838d5263dbd552cf0265e6155ccb32a17e4a
Aidan <aidanpereyrawagner@gmail.com>
2022-06-29 09:14:05 -0400

    responsive nav and some design changes

--- client/src/components/navbarc.jsx
+++ client/src/components/navbarc.jsx
@@ -4,28 +4,52 @@ import {useUserContext} from './UserContext';
 
 const Navbarc = () => {
     const [User, SetUser] = useUserContext();
+
   return (
     <nav>
-    <Link to="/" className="nav-logo">Morix</Link>
-    <div className='links'>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/catalog">Catalog</Link></li>
-      </ul>
-      <ul>
-        <>{ User.auth? <>
-        <li><Link to="/user">Bienvenido {User.UserData.Nombre+'!'}</Link></li>
-        <li><Link to='/logout'>Log Out</Link> </li>
-        </>
-         : <>
-         <li><Link to="/login">Log-in</Link></li>
-         <li><Link to="/SignIn">Register</Link></li>
-         </>
-         } </>
-      </ul>
-    </div>
-  </nav>
+      <div className='default-nav'>
+        <Link to="/" className="nav-logo">Morix</Link>
+        <div className='links'>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/catalog">Catalog</Link></li>
+          </ul>
+          <ul>
+            <>{ User.auth? <>
+            <li><Link to="/user">Bienvenido {User.UserData.Nombre+'!'}</Link></li>
+            <li><Link to='/logout'>Log Out</Link> </li>
+            </>
+            : <>
+            <li><Link to="/login">Log-in</Link></li>
+            <li><Link to="/SignIn">Register</Link></li>
+            </>
+            } </>
+          </ul>
+        </div>
+      </div>
+      <div className='responsive-nav'>
+        <Link to="/" className="nav-logo">Morix</Link>
+        <button class="nav-menu" onClick={responsiveMenuClick}><i class="fa-solid fa-bars"></i></button>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/catalog">Catalog</Link></li>
+          <>{ User.auth? <>
+          <li><Link to="/user">Bienvenido {User.UserData.Nombre+'!'}</Link></li>
+          <li><Link to='/logout'>Log Out</Link> </li>
+          </>
+          : <>
+          <li><Link to="/login">Log-in</Link></li>
+          <li><Link to="/SignIn">Register</Link></li>
+          </>
+          } </>
+        </ul>
+      </div>
+    </nav>
   )
+
+  function responsiveMenuClick() {
+    document.querySelector('.responsive-nav > ul').classList.toggle('active')
+  }
 }
 //SetUser({ auth: false, token: null,UserData:{}})
 export default Navbarc
--- client/src/pages/Index.jsx
+++ client/src/pages/Index.jsx
@@ -4,7 +4,11 @@ function IndexP() {
   return (
     <div className={IndexCSS.index}>
       <section className={IndexCSS.welcome}>
-        
+        <div className={IndexCSS.welcomeText}>
+        <h1 className={IndexCSS.welcomeTitle}>Tu Boleta a un solo click</h1>
+        <p>Y más rápido que nunca gracias a Morix</p>
+        </div>
+        <div className={IndexCSS.welcomeImg}></div>
       </section>
       <section className={IndexCSS.aboutUs}>
 
